a9f5866db9ff7452b7b8188560b1ad87dd317622
EFFY MIN <effymin@gmail.com>
2020-04-14 12:44:49 -0400

    first commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'rack'
+require "'erb'
+
+# start with creating a server
+
+
+
+# server needs routes!
+
+
+
+# initialize app
+
+
+
+# retrieve data objects from Toronto
+
+
+
+# map out data objects to heatmap?
