6d7b50683d6e70064ce8def912a5d75b0058e201
EFFY MIN <effyverse@gmail.com>
2020-04-29 09:12:43 -0400

    google maps api added

--- config.ru
+++ config.ru
@@ -1,31 +1,3 @@
-# Iteration 1: Rewrite this into ruby method
-#
-# run -> env {
-#   [200, {'Content-Type' => 'text/plain'},
-#     ["Your IP: #{env['REMOTE_ADDR']}"]]
-#   }
-
-# what I know:
-# - run is a built-in ruby web server method that expects a rack app
-# as its argument
-# - the lambda above is my app so I am calling run on the env hash
-#
-# - to rewrite this, I therefore need:
-# 1. a run
-# 2. a method has to be called somewhere
-# 3. a method that contains the env hash-like
-# info that the call returns
-
-
-# class RackAppGetRequests
-#
-#   def call(env)
-#     # use env as argument for the call method
-#     [200, {'Content-Type' => 'text/plain'},
-#     ["This is a successful get request!"]]
-#   end
-# end
-
 require 'erb'
 
 class Template
@@ -42,6 +14,14 @@ class Template
   # receive api object
   def receive_api
   end
+
+  # figure out a way to get my api key from hidden
+  def hide_api_key
+  end
+
+  def google_api_call
+  end
+
 end
 
 # Handling request
--- static/scripts.js
+++ static/scripts.js
@@ -1,10 +1,22 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+alert("be present");
 
-  alert("hi cupho!!! YOU GOT THIS!!!");
-  document.getElementById("click").onclick = function() {clickFunction()};
-  function clickFunction() {
-    document.getElementById("show-text").innerHTML = "RENDER TEXT HERE";
-  };
+// document.addEventListener("DOMContentLoaded", function(event) {
+//
+//   document.getElementById("click").onclick = function() {clickFunction()};
+//   function clickFunction() {
+//     document.getElementById("show-text").innerHTML = "RENDER TEXT HERE";
+//   };
+
+  function initMap() {
+// The location of Uluru
+var uluru = {lat: -25.344, lng: 131.036};
+// The map, centered at Uluru
+var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 4, center: uluru});
+// The marker, positioned at Uluru
+var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
 //
 // var url =  "https://data.ontario.ca/dataset/confirmed-positive-cases-of-covid-19-in-ontario";
 // window.location.href = url
@@ -14,4 +26,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
 // bring back the inner html of the set_header
 // and display it when page loads
 
-});
+// });
